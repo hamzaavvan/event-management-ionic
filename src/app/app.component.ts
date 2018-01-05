e7829f196f6aaed9831be62aa5048acf9872187f
Hamza AVvan <hamzaavvan@gmail.com>
2018-01-06 01:13:59 +0500

    status bar updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,9 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      // statusBar.styleDefault();
+      statusBar.overlaysWebView(true);
+      statusBar.backgroundColorByHexString('#2196F3');
       splashScreen.hide();
     });
   }
